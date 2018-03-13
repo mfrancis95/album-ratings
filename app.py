8ae872e3b96f9875dc3f2e97cc0d9422b0ddd910
mfrancis95 <mikefrancis95@gmail.com>
2018-03-13 08:59:45 -0400

    Add route for retrieving albums by decade

--- app.py
+++ app.py
@@ -8,4 +8,15 @@ albums = MongoClient().album_ratings.albums
 
 @app.route('/', methods = ['GET'])
 def all():
-    return jsonify(list(albums.find({}, {'_id': False})))
+    return get_albums()
+
+@app.route('/decade/<decade>', methods = ['GET'])
+def by_decade(decade):
+    try:
+        decade = int(decade[:4]) // 10 * 10
+        return get_albums({'$and': [{'year': {'$gte': decade}}, {'year': {'$lt': decade + 10}}]})
+    except:
+        return get_albums()
+
+def get_albums(filters = {}):
+    return jsonify(list(albums.find(filters, {'_id': False})))
